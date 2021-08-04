b46de8eb3b74fdca476f25611a0e51f0127b5fde
Aaron Buachie <aaron.buachiea@gmail.com>
2021-08-04 11:39:31 -0400

    Application now displays data from array, Complete initial styling, Create toggles for data

---
+++ src/components/DisplayPlanets.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+
+interface Planet {
+  data: {
+    name: string;
+    overview: { content: string; source: string };
+    structure: { content: string; source: string };
+    geology: { content: string; source: string };
+    rotation: string;
+    revolution: string;
+    radius: string;
+    temperature: string;
+    images: { planet: string; internal: string; geology: string };
+  };
+}
+
+const DisplayPlanets = (props: Planet) => {
+  const [overview, toggleOverview] = useState<Boolean>(false);
+  const [internal, toggleInternal] = useState<Boolean>(false);
+  const [geology, toggleGeology] = useState<Boolean>(true);
+
+  const handleToggle = () => {};
+  return (
+    <div className="planet">
+      <div className="planet-data">
+        <div className="img-container">
+          <img
+            className="planet-image"
+            src={
+              internal == true
+                ? process.env.PUBLIC_URL + props.data.images.internal
+                : process.env.PUBLIC_URL + props.data.images.planet
+            }
+          />
+          {geology == true ? (
+            <img
+              className="planet-geology"
+              src={process.env.PUBLIC_URL + props.data.images.geology}
+              alt="geology"
+            />
+          ) : null}
+        </div>
+
+        <div className="planet-info">
+          <h1 className="planet-name">{props.data.name}</h1>
+          <p className="planet-overview">
+            {overview == true
+              ? props.data.overview.content
+              : internal == true
+              ? props.data.structure.content
+              : props.data.geology.content}
+          </p>
+          <p className="source">
+            Source:{" "}
+            <a
+              href={
+                overview == true
+                  ? props.data.overview.source
+                  : internal == true
+                  ? props.data.structure.source
+                  : props.data.geology.source
+              }
+            >
+              Wikipedia
+            </a>
+          </p>
+          <div className="button">
+            01 <span>Overview</span>
+          </div>
+          <div className="button">
+            02 <span>Internal Structure</span>
+          </div>
+          <div className="button">
+            03 <span>Surface Geology</span>
+          </div>
+        </div>
+      </div>
+      <div className="planet-stats">
+        <div className="stat-window">
+          <p>ROTATION TIME</p>
+          <h2>{props.data.rotation}</h2>
+        </div>
+        <div className="stat-window">
+          <p>REVOLUTION TIME</p>
+          <h2>{props.data.revolution}</h2>
+        </div>
+        <div className="stat-window">
+          <p>RADIUS</p>
+          <h2>{props.data.radius}</h2>
+        </div>
+        <div className="stat-window">
+          <p>AVERAGE TEMP.</p>
+          <h2>{props.data.radius}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayPlanets;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,14 +5,38 @@ const Navbar = () => {
     <nav className="nav-bar">
       <h2 className="nav-title">The Planets</h2>
       <div className="nav-menu">
-        <a href="#">Mercury</a>
-        <a href="#">Venus</a>
-        <a href="#">Earth</a>
-        <a href="#">Mars</a>
-        <a href="#">Jupiter</a>
-        <a href="#">Saturn</a>
-        <a href="#">Uranus</a>
-        <a href="#">Neptune</a>
+        <a href="#">
+          {" "}
+          <h3>Mercury</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Venus</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Earth</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Mars</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Jupiter</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Saturn</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Uranus</h3>
+        </a>
+        <a href="#">
+          {" "}
+          <h3>Neptune</h3>
+        </a>
       </div>
     </nav>
   );
