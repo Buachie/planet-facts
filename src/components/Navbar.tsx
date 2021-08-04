d44742dd9e30a6c092ca58f581cf04348f15673b
Aaron Buachie <aaron.buachiea@gmail.com>
2021-08-04 15:49:33 -0400

    Make nav responsive, Add stars to background, Fix image scaling on smaller screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,43 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [open, setOpen] = useState<Boolean>(false);
+
   return (
     <nav className="nav-bar">
       <h2 className="nav-title">The Planets</h2>
-      <div className="nav-menu">
-        <Link to="/">Mercury</Link>
-        <Link to="/venus">Venus</Link>
-        <Link to="/earth">Earth</Link>
-        <Link to="/mars">Mars</Link>
-        <Link to="/jupiter">Jupiter</Link>
-        <Link to="/saturn">Saturn</Link>
-        <Link to="/uranus">Uranus</Link>
-        <Link to="/neptune">Neptune</Link>
+      <div className="menu-icon">
+        <i
+          className={open ? "fas fa-times fa-2x" : "fas fa-bars fa-2x"}
+          onClick={() => setOpen(!open)}
+        ></i>
+      </div>
+      <div className={open ? "nav-menu active" : "nav-menu"}>
+        <Link to="/" onClick={() => setOpen(false)}>
+          Mercury
+        </Link>
+        <Link to="/venus" onClick={() => setOpen(false)}>
+          Venus
+        </Link>
+        <Link to="/earth" onClick={() => setOpen(false)}>
+          Earth
+        </Link>
+        <Link to="/mars" onClick={() => setOpen(false)}>
+          Mars
+        </Link>
+        <Link to="/jupiter" onClick={() => setOpen(false)}>
+          Jupiter
+        </Link>
+        <Link to="/saturn" onClick={() => setOpen(false)}>
+          Saturn
+        </Link>
+        <Link to="/uranus" onClick={() => setOpen(false)}>
+          Uranus
+        </Link>
+        <Link to="/neptune" onClick={() => setOpen(false)}>
+          Neptune
+        </Link>
       </div>
     </nav>
   );
